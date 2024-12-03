3e49de35bf19e0956089b943ef98ada9b47094be
Carlos Lima <a2021135544@isec.pt>
2024-12-03 17:51:36 +0000

    WIP on main

---
+++ logica.js
@@ -0,0 +1,8 @@
+/*window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) { // Se o scroll ultrapassar 50px
+        navbar.classList.add('transparent');
+    } else {
+        navbar.classList.remove('transparent');
+    }
+});*/
